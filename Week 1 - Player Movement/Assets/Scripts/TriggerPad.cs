312514747d7cffd179696bb75e9f40e28834df4f
terrex135 <107088357+sjgame@users.noreply.github.com>
2022-10-09 01:00:10 +1100

    Played around with triggers
    - Tried to clamp the scale of the expanding sphere. - Had no luck

--- Week 1 - Player Movement/Assets/Scripts/TriggerPad.cs
+++ Week 1 - Player Movement/Assets/Scripts/TriggerPad.cs
@@ -2,33 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class TriggerPad : MonoBehaviour
-//public class TriggerPad : MonoBehaviour
-//{
-//    public GameObject sphere;   //The object we wish to change
+public class TriggerPad : MonoBehaviour
 
-//    void OnTriggerEnter(Collider other)
-//    {
-//        if (other.CompareTag("Player"))
-//        {
-//            //change the spheres colour to green
-//        }
-//    }
+{
+    public GameObject sphere;
 
-//    void OnTriggerStay(Collider other)
-//    {
-//        if (other.CompareTag("Player"))
-//        {
-//            //Increas the spheres scale by 0.01 on all axis
-//        }
-//    }
+    public float xMin = 1, xMax =3;
+    public float yMin = 1, yMax =3;
+    public float zMin = 1, zMax =3;
 
-//    void OnTriggerExit(Collider other)
-//    {
-//        if (other.CompareTag("Player"))
-//        {
-//            //set the spheres size back to 1
-//            //Change the spheres colour to yellow
-//        }
-//    }
-//}
+    //float scale = 0f;
+        
+        //The object we wish to change
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        //change the spheres colour to green
+    //    }
+    //}
+
+    void OnTriggerStay(Collider other)
+    {
+        float xValue = sphere.transform.localScale.x;
+        float yValue = sphere.transform.localScale.y;
+        float zValue = sphere.transform.localScale.z;
+       
+        float xScale = Mathf.Clamp(xValue, xMin, xMax);
+        float yScale = Mathf.Clamp(yValue, yMin, yMax);
+        float zScale = Mathf.Clamp(zValue, zMin, zMax);
+
+        sphere.transform.localScale += new Vector3(xScale, yScale, zScale) * 0.01f;
+      
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        
+        
+        sphere.transform.localScale = Vector3.one;
+       
+        
+    }
+}
