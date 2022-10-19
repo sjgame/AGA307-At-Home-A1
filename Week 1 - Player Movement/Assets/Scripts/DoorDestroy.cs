1cab0ef4d028953841c6503b5bb8f3a9e9cb29fb
sjgame <107088357+sjgame@users.noreply.github.com>
2022-10-19 14:03:35 +1100

    Added opening doors
    - If you destroy the targets the doors will open.

--- Week 1 - Player Movement/Assets/Scripts/DoorDestroy.cs
+++ Week 1 - Player Movement/Assets/Scripts/DoorDestroy.cs
@@ -1,67 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class DoorDestroy : MonoBehaviour
 {
-
+    public GameObject target1; 
+    public GameObject target2; 
     public GameObject door1;
     public GameObject door2;
-    List<GameObject> listOfTargets = new List<GameObject>();
+    //List<GameObject> listOfTargets = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        listOfTargets.AddRange(GameObject.FindGameObjectsWithTag("Target"));
-        print(listOfTargets.Count);
-    }
+        //listOfTargets.AddRange(GameObject.FindGameObjectsWithTag("Target"));
+        
+        //print(listOfTargets.Count);
 
-    public void TargetDestroyed(GameObject target)
-    {
-        if (listOfTargets.Contains(target))
-        {
-            listOfTargets.Remove(target);
-        }
-        print(listOfTargets.Count);
-    }
-
-
-    // Update is called once per frame
-   
-
-    public bool AreTargtsDestroyed()
-    {
-        if(listOfTargets.Count <= 0)
-        {
-            return true;
-            
-        }
-        else
-        {
-            return false;
-        }
-
-            
+        
     }
     void Update()
     {
-        //if (true)
-        //{
-        //    Destroy(door1);
-        //    Destroy(door2);
-        //}
-        //if (false)
-        //{
-        //    print(listOfTargets.Count);
-        //}
-        AreTargtsDestroyed();
+        if (!target1 && !target2)
         {
-            if (true)
-            {
-                Destroy(door1);
-            }
+            Destroy(door1);
+            Destroy(door2);
         }
-       
     }
 
+    //public void TargetDestroyed(GameObject target)
+    //{
+    //    if (listOfTargets.Contains(target))
+    //    {
+    //        listOfTargets.Remove(target);
+
+    //    }
+    //    else
+    //    {
+    //        print(listOfTargets.Count);
+    //    }
+        
+    //}
+
+    
+
+    //public void OpenDoor()
+    //{
+    //    if (listOfTargets.Count <= 0)
+    //    {
+    //       Destroy(door1);  
+    //    }
+    //    else 
+    //    {
+    //        print(listOfTargets.Count); 
+    //    }
+    //}
+
+
+    // Update is called once per frame
+
+
+    //public bool AreTargtsDestroyed()
+    //{
+    //    if(listOfTargets.Count <= 0)
+    //    {
+    //        return true;
+
+    //    }
+    //    else
+    //    {
+    //        return false;
+    //    }
+
+
+    //}
 
 }
